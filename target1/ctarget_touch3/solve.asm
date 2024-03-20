102b3a86ee119f9bab4d1de4749a04407377aaa8
HOLIC0105 <3485995896@qq.com>
2024-03-20 14:19:35 +0800

    change

---
+++ target1/ctarget_touch3/solve.asm
@@ -0,0 +1,10 @@
+
+solve.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	bf c8 dc 61 55       	mov    $0x5561dcc8,%edi
+   5:	68 fa 18 40 00       	push   $0x4018fa
+   a:	c3                   	ret    
---
+++ target1/ctarget_touch3/solve.s
@@ -0,0 +1,3 @@
+mov $0x5561dcc8, %edi
+push $0x4018fa
+ret
