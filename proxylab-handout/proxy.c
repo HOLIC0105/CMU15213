2a8d9f047e97666c90a333365b4407c0f111df98
jiaju.tong <jiaju.tong@transwarp.io>
2024-04-19 15:58:37 +0800

    change

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -5,38 +5,28 @@
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 #define	MAXLINE	 8192 
-
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
-
 typedef char string[MAXLINE];
-
 typedef struct{
     string host;
     string port;
     string path;
 }url_t;
-
 void * doit(void * tmpfd);
+int parse(rio_t client_rio, url_t * url_info, string header_info);
 int  parse_url(char *uri, url_t * url_info);
 int parse_header(rio_t* rio, string header_info, string host);
-
-
-int main(int argc, char *argv[])
-{
-    
+int main(int argc, char *argv[]) {
     Signal(SIGPIPE, SIG_IGN);
-
     int listenfd, * connfd;
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
-
     if(argc != 2) {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
 	    exit(1);
     }
-
     listenfd = Open_listenfd(argv[1]);
     pthread_t tpid;
     while(1) {
@@ -50,64 +40,62 @@ int main(int argc, char *argv[])
     }
     return 0;
 }
-
+static char buf[MAXLINE << 2];
+static string method, url, version;
 void * doit(void * tmpfd) {
     pthread_detach(pthread_self());
     int client_fd = *((int *) tmpfd);
     free(tmpfd);
     rio_t client_rio;
-    char buf[MAXLINE << 2];
-    string method, url, version;
+    url_t url_info; 
+    string header_info;
     Rio_readinitb(&client_rio,  client_fd);
-    if(!rio_readlineb(&client_rio, buf, MAXLINE)) {
-        fprintf(stderr, "Parse request line error: %s\n", strerror(errno));
+    if(parse(client_rio, &url_info, header_info) == -1) {
         close(client_fd);
         return NULL;
+    } 
+    int server_fd = open_clientfd(url_info.host, url_info.port);
+    rio_t server_rio;
+    if(server_fd < 0) {
+        fprintf(stderr, "Open connect to %s:%s error\n", url_info.host, url_info.port);
+        return NULL;
+    }
+    rio_readinitb(&server_rio, server_fd);
+    sprintf(buf, "GET %s HTTP/1.0\r\n%s", url_info.path, header_info);
+    if(rio_writen(server_fd, buf, strlen(buf)) != strlen(buf)) {
+        fprintf(stderr, "Send request line and header error\n");
+        close(server_fd);
+        return NULL;
+    }
+    int readlen;
+    while((readlen = rio_readnb(&server_rio, buf, MAXLINE))) {
+        if(readlen < 0) fprintf(stderr, "Read server response error\n");
+        else if(rio_writen(client_fd, buf, readlen) != readlen) 
+            fprintf(stderr, "Send response to client error\n");
+        else continue;
+        close(server_fd);
+        return NULL;
+    }
+    close(server_fd);
+    close(client_fd);
+    return NULL;
+}
+int parse(rio_t client_rio, url_t * url_info, string header_info) {
+    if(!rio_readlineb(&client_rio, buf, MAXLINE)) {
+        fprintf(stderr, "Parse request line error: %s\n", strerror(errno));
+        return -1;
     }
     if(sscanf(buf, "%s %s %s", method, url, version) != 3) {
         fprintf(stderr, "Parse request line error: %s\n", strerror(errno));
-        close(client_fd);
-        return NULL;
+        return -1;
     }
     if(strcasecmp(method, "GET")) {
         fprintf(stderr, "Proxy does not implement the method");
-        close(client_fd);
-        return NULL;
+        return -1;
     }
-    url_t url_info;
-    if(!parse_url(url, &url_info)) {
-        string header_info;
-        parse_header(&client_rio, header_info, url_info.host);
-        int server_fd = open_clientfd(url_info.host, url_info.port);
-        rio_t server_rio;
-        if(server_fd < 0) {
-            fprintf(stderr, "Open connect to %s:%s error\n", url_info.host, url_info.port);
-            return NULL;
-        }
-        rio_readinitb(&server_rio, server_fd);
-        sprintf(buf, "GET %s HTTP/1.0\r\n%s", url_info.path, header_info);
-        if (rio_writen(server_fd, buf, strlen(buf)) != strlen(buf)) {
-            fprintf(stderr, "Send request line and header error\n");
-            close(server_fd);
-            return NULL;
-        }
-        int readlen;
-        while ((readlen = rio_readnb(&server_rio, buf, MAXLINE))) {
-            if (readlen < 0) {
-                fprintf(stderr, "Read server response error\n");
-                close(server_fd);
-                return NULL;
-            }
-            if (rio_writen(client_fd, buf, readlen) != readlen) {
-                fprintf(stderr, "Send response to client error\n");
-                close(server_fd);
-                return NULL;
-            }
-        }
-        close(server_fd);
-    }
-    close(client_fd);
-    return NULL;
+    if(parse_url(url, url_info) == -1) return -1;
+    parse_header(&client_rio, header_info, url_info->host);
+    return 0;
 }
 int parse_url(char * url, url_t * url_info) 
 {
